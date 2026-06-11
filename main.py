dbc1103bff1d3d1c6bb864f60fdeccc678e81d0b
amiralinmp841-alt <amiralinmp841@gmail.com>
2026-06-11 15:57:41 +0330

    Update main.py

--- main.py
+++ main.py
@@ -916,7 +916,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.user_data["current_node"] = "root"
     
     await update.message.reply_text(
-        """🕊 به ربات دانشگاه خوش آمدید. (V_4.3.23)
+        """🕊 به ربات دانشگاه خوش آمدید. (V_4.3.24)
     
     🔍 برای یافتن فایل مورد نظر، میتوانید به صورت متنی سرچ کنید.
     مثل: وویس جلسه اول باکتری شناسی بهمن 403، جزوه فیزیولوژی کلیه و...
